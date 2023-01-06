6f77a7cb1b04b6e6ea8bdc3eeb70bb6cdb5e3480
aashish <aashish.kumar@ioanyt.com>
2023-01-06 17:51:07 +0530

    fix error

--- src/main/java/com/entity/org/entity/example/security/WebSecurityConfig.java
+++ src/main/java/com/entity/org/entity/example/security/WebSecurityConfig.java
@@ -1,10 +1,10 @@
-package com.entity.org.entity.example.security;
-
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@Configurable
-@EnableWebSecurity
-public class WebSecurityConfig {
-
-}
+//package com.entity.org.entity.example.security;
+//
+//import org.springframework.beans.factory.annotation.Configurable;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@Configurable
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
+//}
